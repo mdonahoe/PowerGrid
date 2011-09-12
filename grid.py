4db7a79a1693af30a737f90395ea477bb17223e9
Matt Donahoe <matt@pocketgems.com>
2011-09-12 10:15:38 -0700

    fix cost calculation. prevent double purchasing of cities

--- grid.py
+++ grid.py
@@ -10,6 +10,15 @@ class City(object):
         #0:10
         #1:15
         #2:20
+    def buy(self,player):
+        assert(self.can_buy())
+        self.spots.append(player)
+    def can_buy(self):
+        return len(self.spots)==0
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return "City('%s')" % self.name
 
 class Grid(object):
     def __init__(self,colors):
@@ -26,15 +35,22 @@ class Grid(object):
             self.graph[c] = {}
 
         #attach edges
-        for node in constants.network:
-            for neighbor,cost in constants.network[c].iteritems():
+        for node in self.cities:
+            if node not in constants.network: continue
+            for neighbor,cost in constants.network[node].iteritems():
                 if neighbor not in self.cities: continue
                 self.graph[node][neighbor] = cost
                 self.graph[neighbor][node] = cost
+
+    def __str__(self):
+        return '\n\t'.join(['Cities']+self.cities.keys())
     def _costs(self,owned_cities):
         #assume I can have many roots
-        costs = dict((c,0) for c in owned_cities)
-        queue = owned_cities[:]
+        if not owned_cities:
+            costs = dict((c,0) for c in self.cities)
+            return costs
+        costs = dict((c.name,0) for c in owned_cities)
+        queue = [c.name for c in owned_cities[:]]
         while queue:
             node = queue.pop(0)
             for neighbor,cost in self.graph[node].iteritems():
@@ -45,15 +61,17 @@ class Grid(object):
         return costs
     def price_for_city(self,cityname,owned_cities):
         city = self.cities[cityname]
-        return self._costs(owned_cities)[cityname] + city.price()
-    def cheapest(self,owned_cities):
+        cs = self._costs(owned_cities)
+        return cs[cityname] + city.price()
+    def price_sorted(self,owned_cities):
         costs = self._costs(owned_cities)
         prices = []
-        for city,cost in costs.iteritems():
-            if city in owned_cities: continue
-            prices.append((cost + self.cities[city].price(),city))
+        for name,cost in costs.iteritems():
+            city = self.cities[name]
+            if not city.can_buy(): continue
+            prices.append((cost + city.price(), name))
         prices.sort()
-        return prices[0]
+        return prices
     def bfs(self,root):
         """breadth-first search algorithm"""
         prev = {}
