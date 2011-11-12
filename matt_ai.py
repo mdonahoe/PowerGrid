09f0764b2f588e702dc2c467f600faa2c8819b75
matt.donahoe@gmail.com <matt.donahoe@gmail.com>
2011-11-12 14:24:55 -0800

    matt ai

--- matt_ai.py
+++ matt_ai.py
@@ -15,10 +15,21 @@ class MattAI(player.SafePlayer):
 
     def _redistribute_resources(self,rs):
         """move resources to other power plants and return overflow"""
+        for plant in reversed(self.power_plants):
+            rs = plant.better_stock(rs)
+
         return rs
 
     def _initial_bid(self, pp_market, bidders):
         """bid on the plant with the lowest $/city"""
+        if len(self.cities) < self.total_capacity():
+            return None
+        for plant in reversed(pp_market.actual()):
+            if plant.price > self.money:
+                continue
+            if len(self.power_plants) == 4 and plant.price < min([x.price for x in self.power_plants]):
+                return None
+            return plant.price, plant
         return None
 
     def _get_bid(self,price,plant,bidders):
@@ -26,11 +37,31 @@ class MattAI(player.SafePlayer):
 
     def _buy_resources(self, resource_market):
         """Buy enough to power all your plants"""
-        pass
+        for plant in reversed(self.power_plants):
+            n = plant.resources_needed()
+            if n <= 0:
+                continue
+            resource = sorted(plant.store.keys())[0]
+            print '\tneed %s %s' % (n, resource)
+            if self.buy_resources(resource_market, {resource: n}):
+                plant.stock({resource: n})
 
     def _build_cities(self, grid):
         """Buy as many cities as you can power"""
-        pass
+        while True:
+            cities = grid.price_sorted(self)
+            #print cities[:2], self.name, self.money
+            if not cities:
+                #print self.cities
+                #print set([city.name for city in self.cities])-set(grid.cities.keys())
+                return
+            price, name = cities[0]
+            city = grid.cities[name]
+            if price > self.money or len(self.cities) >= self.total_capacity():
+                break
+            self.buy_city(city, price)
+            print '\t%s for %s' % (name, price)
+        print '\t$%s left' % self.money
 
     def _power_plants_to_use(self):
         """
@@ -38,5 +69,27 @@ class MattAI(player.SafePlayer):
         power from most expensive down
         until we can power all cities
         """
-        return []
+        prs = []
+        powered = 0
+        cities = len(self.cities)
+        for plant in self.power_plants:
+            if 'eco' in plant.store:
+                prs.append((plant, {}))
+                powered += plant.capacity
+        for plant in reversed(self.power_plants):
+            if powered >= cities:
+                print self.name, prs
+                return prs
+            if 'eco' in plant.store:
+                continue
+            if not plant.can_power():
+                continue
+            powered += plant.capacity
+            rs = {}
+            for r in sorted(plant.store.keys()):
+                # take as much as we need/have
+                rs[r] = min(plant.rate - sum(rs.values()), plant.store[r])
+            prs.append((plant, rs))
+        print self.name, prs
+        return prs
 
